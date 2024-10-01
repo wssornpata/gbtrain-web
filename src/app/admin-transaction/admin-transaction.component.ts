c5dee2cb88024178b2add1dfd29231f43260f4d3
AzureAD\Employee0724 <sornpata@wisesoft.co.th>
2024-10-01 16:31:46 +0700

    update admin-transaction

--- src/app/admin-transaction/admin-transaction.component.ts
+++ src/app/admin-transaction/admin-transaction.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { HttpClientModule, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TransactionModel } from '../model/transaction-model';
@@ -11,31 +15,23 @@ import { DateService } from '../services/date.service';
 @Component({
   selector: 'app-admin-transaction',
   standalone: true,
-  imports: [
-    HttpClientModule, 
-    CommonModule, 
-    FormsModule, 
-    PaginationModule,
-  ],
-  providers: [
-    TransactionService,
-    DateService,
-    DatePipe,
-    HttpService,
-  ],
+  imports: [HttpClientModule, CommonModule, FormsModule, PaginationModule],
+  providers: [TransactionService, DateService, DatePipe, HttpService],
   templateUrl: './admin-transaction.component.html',
   styleUrl: './admin-transaction.component.css',
 })
 export class AdminTransactionComponent implements OnInit {
   transactions: TransactionModel[] = [];
+  filteredTransactions: TransactionModel[] = [];
   currentPage: number = 1;
   itemsPerPage: number = 10;
   totalItems: number = 0;
+  searchCriteria: string = '';
 
   constructor(
     private transactionService: TransactionService,
     public dateService: DateService,
-    private httpService: HttpService,
+    private httpService: HttpService
   ) {}
 
   ngOnInit(): void {
@@ -47,19 +43,32 @@ export class AdminTransactionComponent implements OnInit {
       (response: HttpResponse<any>) => {
         if (this.httpService.isResponseOk(response.status)) {
           this.transactions = response.body;
-          this.totalItems = this.transactions.length;
+          this.applyFilter();
         }
       },
       (httpErrorResponse: HttpErrorResponse) => {
-        console.error('Error loading types:', httpErrorResponse);
+        console.error('Error loading transactions:', httpErrorResponse);
       }
     );
+  }
+
+  applyFilter(): void {
+    if (this.searchCriteria) {
+      this.filteredTransactions = this.transactions.filter(
+        (transaction) =>
+          transaction.source.includes(this.searchCriteria) ||
+          transaction.destination.includes(this.searchCriteria)
+      );
+    } else {
+      this.filteredTransactions = this.transactions;
+    }
+    this.totalItems = this.filteredTransactions.length;
   }
 
   get paginatedTransactions(): TransactionModel[] {
     const start = (this.currentPage - 1) * this.itemsPerPage;
     const end = start + this.itemsPerPage;
-    return this.transactions.slice(start, end);
+    return this.filteredTransactions.slice(start, end);
   }
 
   pageChanged(event: any): void {
